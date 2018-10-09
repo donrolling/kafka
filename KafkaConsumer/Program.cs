4f3968651c8d9bc0fa2050547bc0ab8647ffd391
Don <donrolling@gmail.com>
2018-10-09 11:44:04 -0500

    removed old proj not needed

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -7,12 +7,12 @@ namespace KafkaConsumer {
 	internal class Program {
 
 		private static void Main(string[] args) {
-			Console.WriteLine($"Waiting for new messages.");
 			var consumer = KafkaShared.Utility.GetConsumer(KafkaShared.Utility.Topic, (Message<Null, string> msg) => {
 				Console.WriteLine($"Message received: { msg.Value }");
 				Console.WriteLine($"Waiting for new messages.");
 			});
-			Console.WriteLine("Ctrl-C to exit.");
+			Console.WriteLine("Ctrl-C to exit.\r\n");
+			Console.WriteLine($"Waiting for messages.");
 			// Handle Cancel Keypress 
 			var cancelled = false;
 			Console.CancelKeyPress += (_, e) => {
@@ -22,8 +22,6 @@ namespace KafkaConsumer {
 			// Poll for messages
 			while (!cancelled) {
 				consumer.Poll(-1);
-				Thread.Sleep(1000);
-				Console.WriteLine("Waiting for new message...");
 			}
 		}
 	}
